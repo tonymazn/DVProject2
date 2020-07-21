508f4f9d528615eded4ee4172794aba47c1b796a
Tony Ma <tonymazn@gmail.com>
2020-07-21 00:14:39 -0600

    Main - working Pagination

--- js/main.js
+++ js/main.js
@@ -2,41 +2,52 @@ var provincesList = ["British Columbia", "Alberta", "Saskatchewan", "Manitoba", 
 
 
 $(document).ready(function () {
-    $(".page2").empty();
-    $(".page3").empty();
-    $(".main").load("page1.html")
-    $.getScript("js/page1.js");
-});
+    $(".page1").hide();
+    $(".page2").hide();
+    $(".page3").hide();
 
-$('#pagination-here').bootpag({
-    total: 3,          // total pages
-    page: 1,            // default page
-    maxVisible: 3,     // visible pagination
-    leaps: true         // next/prev leaps through maxVisible
-}).on("page", function (event, num) {
-    $("#content").html("Page " + num); // or some ajax content loading...
-
-    if (num == 1) {
-        $(".page2").empty();
-        $(".page3").empty();
-        $(".main").load("page1.html")
+    $(".page1").load("page1.html", function () {
         $.getScript("js/page1.js");
-    }
-    if (num == 2) {
-        $(".page1").empty();
-        $(".page3").empty();
-        $(".main").load("page2.html")
+        return true;
+    });
+    $(".page2").load("page2.html", function () {
         $.getScript("js/page2.js");
-    }
-    if (num == 3) {
-        $(".page1").empty();
-        $(".page2").empty();
-        $(".main").load("page3.html")
-        $.getScript("js/page3.js");
-    }
+        return true;
+    });
+
+    $(".page2").hide();
+    $(".page3").hide();
+    $(".page1").show();
+
+    $('#pagination-here').bootpag({
+        total: 3,           // total pages
+        page: 1,            // default page
+        maxVisible: 3,      // visible pagination
+        leaps: true         // next/prev leaps through maxVisible
+    }).on("page", function (event, num) {
+        $("#content").html("Page " + num); // or some ajax content loading...
+
+        if (num == 1) {
+            $(".page2").hide();
+            $(".page3").hide();
+            $(".page1").show();
+        }
+        if (num == 2) {
+            $(".page2").show();
+            $(".page3").hide();
+            $(".page1").hide();
+        }
+        if (num == 3) {
+            $(".page1").hide();
+            $(".page2").hide();
+            $(".page3").show();
+        }
 
 
-    // ... after content load -> change total to 3
-    $(this).bootpag({ total: 3, maxVisible: 3 });
+        // ... after content load -> change total to 3
+        $(this).bootpag({ total: 3, maxVisible: 3 });
+    });
+
+
 });
 
