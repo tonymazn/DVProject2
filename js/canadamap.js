c95beb15598d55a7cc49cfe2524934a2a56ef12e
Tony Ma <tonymazn@gmail.com>
2020-07-23 00:27:39 -0600

    Page3 - display map - issue: didn't exit the old number - add the colors

--- js/canadamap.js
+++ js/canadamap.js
@@ -3,6 +3,11 @@
 *    Project 2 - ZM11 (Zhouning Ma)
 */
 
+var canadamap_color;
+var canadamap_svg;
+var canadamap_mapdata;
+var page3_titles;
+
 CanadaMap = function () {
     console.log("start CanadaMap");
     this.initVis();
@@ -16,7 +21,7 @@ CanadaMap.prototype.initVis = function () {
     var height = 600 - margin.top - margin.bottom,
         width = 900 - margin.left - margin.right;
 
-    var svg = d3.select(".page3")
+    canadamap_svg = d3.select(".page3")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom);
@@ -28,17 +33,17 @@ CanadaMap.prototype.initVis = function () {
         .domain([1, 10])
         .rangeRound([height, width]);
 
-    var color = d3.scaleThreshold()
+    canadamap_color = d3.scaleThreshold()
         .domain(d3.range(2, 10))
         .range(d3.schemeBlues[9]);
 
-    var g = svg.append("g")
+    var g = canadamap_svg.append("g")
         .attr("class", "key")
         .attr("transform", "translate(50,40)");
 
     g.selectAll("rect")
-        .data(color.range().map(function (d) {
-            d = color.invertExtent(d);
+        .data(canadamap_color.range().map(function (d) {
+            d = canadamap_color.invertExtent(d);
             if (d[0] == null) d[0] = x.domain()[0];
             if (d[1] == null) d[1] = x.domain()[1];
             return d;
@@ -47,7 +52,7 @@ CanadaMap.prototype.initVis = function () {
         .attr("height", 8)
         .attr("x", function (d) { return x(d[0]); })
         .attr("width", function (d) { return x(d[1]) - x(d[0]); })
-        .attr("fill", function (d) { return color(d[0]); });
+        .attr("fill", function (d) { return canadamap_color(d[0]); });
 
     g.append("text")
         .attr("class", "caption")
@@ -61,7 +66,7 @@ CanadaMap.prototype.initVis = function () {
     g.call(d3.axisBottom(x)
         .tickSize(13)
         .tickFormat(function (x, i) { return i ? x : x + "%"; })
-        .tickValues(color.domain()))
+        .tickValues(canadamap_color.domain()))
         .select(".domain")
         .remove();
 
@@ -74,32 +79,40 @@ CanadaMap.prototype.initVis = function () {
     ]
 
     Promise.all(promises).then(function (data) {
-        vis.ready(svg, color, data[0]);
+        canadamap_mapdata = data[0];
+        console.log("canadamap_mapdata");
+        console.log(canadamap_mapdata);
+        vis.updateVis(null);
     }).catch(function (error) {
         console.log(error);
     });
 
 }
 
-CanadaMap.prototype.ready = function (svg, color, canada) {
-    console.log(canada);
+
+CanadaMap.prototype.updateVis = function (data) {
     var path = d3.geoPath();
-    svg.append("g")
+    canadamap_svg.append("g")
         .attr("class", "counties")
         .selectAll("path")
-        .data(topojson.feature(canada, canada.objects.nation).features)
+        .data(topojson.feature(canadamap_mapdata, canadamap_mapdata.objects.nation).features)
         .enter().append("path")
-        .attr("fill", function (d) { return color(d.rate = 3); })
+        .attr("fill", function (d) { return canadamap_color(d.rate = 3); })
         .attr("d", path)
         .append("title")
         .text("31234");
-        //.text(function (d) { return 2.0 + "%"; });
+    //.text(function (d) { return 2.0 + "%"; });
 
-    svg.append("path")
-        .datum(topojson.mesh(canada, canada.objects.prov, function (a, b) { return a !== b; }))
+
+    console.log("canadamap_mapdata");
+    console.log(canadamap_mapdata);
+
+
+    canadamap_svg.append("path")
+        .datum(topojson.mesh(canadamap_mapdata, canadamap_mapdata.objects.prov, function (a, b) { return a !== b; }))
         .attr("class", "states")
         .attr("d", path);
-    var continents = topojson.feature(canada, canada.objects.prov).features;
+    var continents = topojson.feature(canadamap_mapdata, canadamap_mapdata.objects.prov).features;
     var centroids = [
         [260, 460], //British Columbia
         [690, 460], //Quebec
@@ -112,10 +125,11 @@ CanadaMap.prototype.ready = function (svg, color, canada) {
         [790, 550], //New Brunswick
         [370, 350], //Northwest Territories
         [340, 440], //Alberta
-        [800, 420]]; //Newfoundland and Labrador
+        [800, 420], //Newfoundland and Labrador
+        [820, 580]]; //Newfoundland and Labrador
 
 
-    svg.selectAll(".name").data(centroids)
+    canadamap_svg.selectAll(".name").data(centroids)
         .enter().append("text")
         .attr("x", function (d) { return d[0]; })
         .attr("y", function (d) { return d[1]; })
@@ -123,17 +137,22 @@ CanadaMap.prototype.ready = function (svg, color, canada) {
         .attr("text-anchor", "middle")
         .text(function (d, i) { return continents[i].properties.province; });
 
+    if (data) {
+        canadamap_svg.exit()
+            .attr("class", "numbertitle")
+            .remove();
+        canadamap_svg.selectAll(".name").data(centroids)
+            .enter().append("text")
+            .attr("class", "numbertitle")
+            .attr("x", function (d) { return d[0]; })
+            .attr("y", function (d) { return d[1] + 15; })
+            .style("fill", "black")
+            .attr("font-weight", "bold")
+            .attr("font-family", "Open Sans")
+            .attr("text-anchor", "middle")
+            .text(function (d, i) { return common_formatnumber(data[i]); });
 
-    svg.selectAll(".name").data(centroids)
-        .enter().append("text")
-        .attr("x", function (d) { return d[0]; })
-        .attr("y", function (d) { return d[1] + 15; })
-        .style("fill", "black")
-        .attr("font-weight", "bold")
-        .attr("font-family", "Open Sans")
-        .attr("text-anchor", "middle")
-        .text(function (d, i) { return common_formatnumber("8000"); });
-
+    }
 
 }
 
--- js/page3.js
+++ js/page3.js
@@ -3,11 +3,13 @@
 *    Project 2 - ZM11 (Zhouning Ma)
 */
 var page3_canadamap;
+var page3_data = [];
 
 Page3 = function (_parentElement, _parameters) {
     this._parentElement = _parentElement;
 
     this.initVis(_parentElement, _parameters);
+    return this;
 }
 
 Page3.prototype.initVis = function (_parentElement, _parameters) {
@@ -24,14 +26,33 @@ Page3.prototype.initVis = function (_parentElement, _parameters) {
         if (_parameters.endDate < _parameters.startDate) {
             _parameters.startDate = _parameters.minDate;
         }
-        page3Update(_parentElement, _parameters)
+        page3Update(_parameters)
     });
 
     $("#type-select").on('change', function () {
         _parameters.type = $("#type-select").val();
-        page3Update(_parentElement, _parameters)
+        page3Update(_parameters)
     });
-    page3Update(_parameters);
+
+    d3.csv("data/canadacovid19.csv").then(function (data) {
+        console.log("read data");
+        console.log(data);
+
+        data.forEach(function (d) {
+            var item = [];
+            item.numconf = +d.numconf
+            item.numdeaths = +d.numdeaths
+            item.numtested = +d.numtested
+            item.prname = d.prname
+            item.date = common_parseTime(d.date)
+            item.team = d.prname
+            page3_data.push(item);
+        });
+
+        page3Update(_parameters);
+
+    });
+
 }
 
 
@@ -39,13 +60,63 @@ Page3.prototype.trigger = function (_parameters) {
     _parameters.currentPage = 3;
 
     $("#type-select").val(_parameters.type);
-    $("#datepicker").datepicker("setDate",_parameters.endDate);
+    $("#datepicker").datepicker("setDate", _parameters.endDate);
 
 }
 
 
+
+function page3GetData(date, type) {
+    var formatteddate = common_formatTime(date);
+    var records = page3_data.filter(function (d) { return common_formatTime(d.date) == formatteddate; });
+    var result = [];
+    for (var i = 0; i < 13; i++) {
+        if (type == "numconf") {
+            result.push(records[i].numconf);
+        } else if (type == "numdeaths") {
+            result.push(records[i].numdeaths);
+        } else {
+            result.push(records[i].numtested);
+        }
+    };
+
+    var reorderResult = new Array(13);
+    reorderResult[0] = result[0]; //British Columbia
+    reorderResult[10] = result[1];  //Alberta
+    reorderResult[4] = result[2]; //Saskatchewan
+    reorderResult[6] = result[3]; //Manitoba
+    reorderResult[7] = result[4]; //Ontario
+    reorderResult[1] = result[5]; //Quebec
+    reorderResult[11] = result[6]; //Newfoundland and Labrador
+    reorderResult[8] = result[7]; //New Brunswick
+
+    reorderResult[12] = result[8]; //Northwest Territories
+
+
+    reorderResult[3] = result[9]; //Prince Edward Island
+    reorderResult[5] = result[10]; //Yukon
+    reorderResult[9] = result[11]; //Northwest Territories
+    reorderResult[2] = result[12]; // Nunavut
+
+
+
+//    console.log("result[12]");
+//    console.log(result[12]);
+    return reorderResult;
+}
+
+
+
 function page3Update(_parameters) {
+    var endDate = _parameters.endDate;
+    var type = _parameters.type;
 
+    console.log(endDate);
+    console.log(type);
 
+    var data = page3GetData(endDate, type);
+    console.log("data");
+    console.log(data);
+    page3_canadamap.updateVis(data);
 }
 
